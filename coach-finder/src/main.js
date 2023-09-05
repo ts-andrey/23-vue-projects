1cf4a20960271176a97292f23912eb197aa99227
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2023-09-05 17:41:11 +0300

    feat: add basic pages

--- coach-finder/src/main.js
+++ coach-finder/src/main.js
@@ -1,9 +1,11 @@
 import './assets/main.css';
 
 import { createApp } from 'vue';
+
+import App from './App.vue';
 import router from './router';
 
-const app = createApp({});
+const app = createApp(App);
 
 app.use(router);
 
--- coach-finder/src/router.js
+++ coach-finder/src/router.js
@@ -1,14 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import CoachDetails from './pages/coaches/CoachDetails.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceive from './pages/requests/RequestsReceive.vue';
+
+import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
-    { path: '/coaches', component: null },
-    { path: '/coaches/:id', component: null, children: [{ path: '/contact', component: null }] },
-    { path: '/register', component: null },
-    { path: '/requests', component: null },
-    { path: '/:notFound(.*)', component: null },
+    { path: '/coaches', component: CoachesList },
+    { path: '/coaches/:id', component: CoachDetails, children: [{ path: 'contact', component: ContactCoach }] },
+    { path: '/register', component: CoachRegistration },
+    { path: '/requests', component: RequestsReceive },
+    { path: '/:notFound(.*)', component: NotFound },
   ],
 });
 
