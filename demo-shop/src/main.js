3284358c8893f49d8feaebbdb662662ed178c3dc
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2023-09-05 15:24:01 +0300

    feat: add vuex to the project

--- demo-shop/src/main.js
+++ demo-shop/src/main.js
@@ -4,11 +4,14 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import router from './router';
+import store from './store/index';
+
 import BaseBadge from './components/ui/BaseBadge.vue';
 
 const app = createApp(App);
 
 app.use(router);
+app.use(store);
 
 app.component('base-badge', BaseBadge);
 
