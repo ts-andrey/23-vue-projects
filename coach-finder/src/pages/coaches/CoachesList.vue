c319929727282fb1d513d114025dcdde5bba3eaa
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2023-09-06 14:12:42 +0300

    feat: add coaches basic state

--- coach-finder/src/pages/coaches/CoachesList.vue
+++ coach-finder/src/pages/coaches/CoachesList.vue
@@ -5,12 +5,24 @@
       <button>Refresh</button>
       <router-link to="/register">Register as Coach</router-link>
     </div>
-    <ul>
-      List Of Coaches
+    <ul v-if="hasCoaches">
+      <li v-for="coach in filteredCoaches" :key="coach.id">{{ coach }}</li>
     </ul>
+    <h3 v-else>No Coaches found.</h3>
   </section>
 </template>
 
-<script></script>
+<script>
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+};
+</script>
 
 <style scoped></style>
