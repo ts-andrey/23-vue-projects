a27a6dcbdae98f06b070e312428b93e5eca4c9bd
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2023-09-06 14:59:03 +0300

    feat: add few basic components: card, badge, button

--- coach-finder/src/main.js
+++ coach-finder/src/main.js
@@ -6,9 +6,17 @@ import App from './App.vue';
 import router from './router';
 import store from './store/index';
 
+import BaseBadge from './components/ui/BaseBadge.vue';
+import BaseButton from './components/ui/BaseButton.vue';
+import BaseCard from './components/ui/BaseCard.vue';
+
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
+
+app.component('base-badge', BaseBadge);
+app.component('base-button', BaseButton);
+app.component('base-card', BaseCard);
 
 app.mount('#app');
