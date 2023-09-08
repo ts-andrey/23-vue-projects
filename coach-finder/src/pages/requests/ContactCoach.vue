4cbbe97fdcce348aea021dc7b095d1a9521fb663
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2023-09-08 14:58:24 +0300

    feat: add contact form

--- coach-finder/src/pages/requests/ContactCoach.vue
+++ coach-finder/src/pages/requests/ContactCoach.vue
@@ -1,6 +1,81 @@
 <template>
+  <form @submit.prevent="submitForm">
+    <div class="form-control">
+      <label for="email">Your e-mail</label>
+      <input name="email" type="email" id="email" v-model.trim="email" />
+    </div>
+    <div class="form-control">
+      <label for="message">Message</label>
+      <textarea name="message" id="message" cols="30" rows="5" v-model.trim="message"></textarea>
+    </div>
+    <p v-if="!formIsValid">Please enter a valid e-mail and non-empty message!</p>
+    <div class="actions">
+      <base-button>Send Message</base-button>
+    </div>
+  </form>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      message: '',
+      formIsValid: true,
+    };
+  },
+  methods: {
+    submitForm() {
+      this.formIsValid = true;
+      if (this.email === '' || !this.email.includes('@') || this.message === '') {
+        this.formIsValid = false;
+        return;
+      }
+    },
+  },
+};
+</script>
 
-<style scoped></style>
+<style scoped>
+form {
+  margin: 1rem;
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  display: block;
+  margin-bottom: 0.5rem;
+  font-weight: bold;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+  font: inherit;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+
+.errors {
+  color: #ff0000;
+  font-weight: bold;
+}
+
+.actions {
+  text-align: center;
+}
+</style>
