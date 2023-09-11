c0f245efdd5720202e0d99bf8cf30693ccd18d4c
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2023-09-11 23:24:05 +0300

    feat: add 404 page

--- coach-finder/src/pages/NotFound.vue
+++ coach-finder/src/pages/NotFound.vue
@@ -1,4 +1,14 @@
-<template></template>
+<template>
+  <section>
+    <base-card>
+      <h2>404 error!</h2>
+      <p>
+        This page could not be found - maybe check out all our coaches
+        <router-link to="/coaches">coaches</router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
 
 <script></script>
 
