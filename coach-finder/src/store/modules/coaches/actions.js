a1c48e378a7cf3e79878418c685a51083fd6e956
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2023-09-11 19:25:57 +0300

    feat: add some cashing for fetching coaches data

--- coach-finder/src/store/modules/coaches/actions.js
+++ coach-finder/src/store/modules/coaches/actions.js
@@ -25,7 +25,11 @@ export default {
     }
   },
 
-  async loadCoaches(context) {
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
+
     const response = await fetch(
       `https://coach-finder-ants-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`
     );
@@ -51,6 +55,7 @@ export default {
       }
 
       context.commit('setCoaches', coaches);
+      context.commit('setFetchTimeStamp');
     }
   },
 };
--- coach-finder/src/store/modules/coaches/mutations.js
+++ coach-finder/src/store/modules/coaches/mutations.js
@@ -6,4 +6,8 @@ export default {
   setCoaches(state, payload) {
     state.coaches = payload;
   },
+
+  setFetchTimeStamp(state){
+    state.lastFetch = new Date().getTime();
+  }
 };
