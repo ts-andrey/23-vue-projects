529896a6120722b2d7392dc3962b9cbc36c6e97a
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2023-09-11 23:19:01 +0300

    feat: add route transition

--- coach-finder/src/pages/coaches/CoachDetails.vue
+++ coach-finder/src/pages/coaches/CoachDetails.vue
@@ -1,25 +1,27 @@
 <template>
-  <section>
-    <base-card>
-      <h2>{{ fullName }}</h2>
-      <h3>${{ rate }}/hour</h3></base-card
-    >
-  </section>
-  <section>
-    <base-card>
-      <header>
-        <h2>Interested? Reach out Now!</h2>
-        <base-button link :to="contactLink">Contact</base-button>
-      </header>
-      <router-view></router-view>
-    </base-card>
-  </section>
-  <section>
-    <base-card>
-      <base-badge v-for="area in areas" :key="area.id" :type="area" :title="area"></base-badge>
-      <p>{{ description }}</p></base-card
-    >
-  </section>
+  <div>
+    <section>
+      <base-card>
+        <h2>{{ fullName }}</h2>
+        <h3>${{ rate }}/hour</h3></base-card
+      >
+    </section>
+    <section>
+      <base-card>
+        <header>
+          <h2>Interested? Reach out Now!</h2>
+          <base-button link :to="contactLink">Contact</base-button>
+        </header>
+        <router-view></router-view>
+      </base-card>
+    </section>
+    <section>
+      <base-card>
+        <base-badge v-for="area in areas" :key="area.id" :type="area" :title="area"></base-badge>
+        <p>{{ description }}</p></base-card
+      >
+    </section>
+  </div>
 </template>
 
 <script>
@@ -44,7 +46,7 @@ export default {
       return this.selectedCoach.description;
     },
     contactLink() {
-      return `${this.$route.path}/${this.id}/contact`;
+      return `${this.$route.path}/contact`;
     },
   },
   created() {
