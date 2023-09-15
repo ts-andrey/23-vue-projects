b1e78f7ab42eb0f0201cd199387f9752eec45346
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2023-09-15 17:40:43 +0300

    feat: add little optimization

--- coach-finder/src/main.js
+++ coach-finder/src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css';
 
-import { createApp } from 'vue';
+import { createApp, defineAsyncComponent } from 'vue';
 
 import App from './App.vue';
 import router from './router';
@@ -10,7 +10,8 @@ import BaseBadge from './components/ui/BaseBadge.vue';
 import BaseButton from './components/ui/BaseButton.vue';
 import BaseCard from './components/ui/BaseCard.vue';
 import BaseSpinner from './components/ui/BaseSpinner.vue';
-import BaseDialog from './components/ui/BaseDialog.vue';
+
+const BaseDialog = defineAsyncComponent(() => import('./components/ui/BaseDialog.vue'));
 
 const app = createApp(App);
 
--- coach-finder/src/router.js
+++ coach-finder/src/router.js
@@ -2,16 +2,20 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import store from './store/index';
 
-import CoachDetails from './pages/coaches/CoachDetails.vue';
 import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+// import CoachDetails from './pages/coaches/CoachDetails.vue';
+// import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+// import ContactCoach from './pages/requests/ContactCoach.vue';
+// import RequestsReceived from './pages/requests/RequestsReceived.vue';
+// import UserAuth from './pages/auth/UserAuth.vue';
+// import NotFound from './pages/NotFound.vue';
 
-import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestsReceived from './pages/requests/RequestsReceived.vue';
-
-import UserAuth from './pages/auth/UserAuth.vue';
-
-import NotFound from './pages/NotFound.vue';
+const CoachDetails = () => import('./pages/coaches/CoachDetails.vue');
+const CoachRegistration = () => import('./pages/coaches/CoachRegistration.vue');
+const ContactCoach = () => import('./pages/requests/ContactCoach.vue');
+const RequestsReceived = () => import('./pages/requests/RequestsReceived.vue');
+const UserAuth = () => import('./pages/auth/UserAuth.vue');
+const NotFound = () => import('./pages/NotFound.vue');
 
 const router = createRouter({
   history: createWebHistory(),
