5f94a4f244060675dd9d43946a2b830d058ec0bc
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2023-09-15 14:15:33 +0300

    feat: add logout logic

--- coach-finder/src/store/modules/auth/actions.js
+++ coach-finder/src/store/modules/auth/actions.js
@@ -48,4 +48,11 @@ export default {
       tokenExpiration: responseData.expiresIn,
     });
   },
+  logout(context) {
+    context.commit('setUser', {
+      token: null,
+      userId: null,
+      tokenExpiration: null,
+    });
+  },
 };
