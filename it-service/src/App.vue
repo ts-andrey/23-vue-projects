7f03b82cc1211fa39577de19d8b9bdf3b6007065
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2023-09-22 17:44:18 +0300

    feat: add header component with basic navigation

--- it-service/src/App.vue
+++ it-service/src/App.vue
@@ -1,7 +1,15 @@
 <template>
+  <TheHeader />
   <router-view></router-view>
 </template>
 
-<script></script>
+<script>
+import TheHeader from './components/header/TheHeader.vue';
+export default {
+  components: {
+    TheHeader,
+  },
+};
+</script>
 
 <style scoped></style>
---
+++ it-service/src/components/header/TheHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <ul>
+      <router-link to="/">Home</router-link>
+      <router-link to="/phones">Phones</router-link>
+      <router-link to="/computers">Computers</router-link>
+      <router-link to="/service">Service</router-link>
+      <router-link to="/websites">Websites</router-link>
+    </ul>
+  </nav>
+</template>
+
+<script></script>
+
+<style scoped></style>
