f5db569d1dc0b8ea3f85a8b1007779f63e40bc0c
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2023-10-07 00:48:19 +0300

    feat: add web page content

--- it-service/src/components/websites/WebSites.vue
+++ it-service/src/components/websites/WebSites.vue
@@ -1,16 +1,44 @@
 <template>
   <section class="web">
+    <h1>We provide web developing services</h1>
+    <p>
+      <span> We are able to develop any kind of resource for you </span>
+      <span>(website, server, API, desktop, mobile, widget, ADS)</span>
+    </p>
+    <h2>We are successfully developed the following recources:</h2>
+    <ul>
+      <li v-for="web in webData">
+        <a :href="web.link" target="_blank">
+          <h3>{{ web.name }}</h3>
+          <img :src="web.img" :alt="web.name" />
+        </a>
+      </li>
+    </ul>
     <video class="video" src="../../assets/video/web-bg-video.mp4" muted autoplay loop></video>
   </section>
 </template>
 
-<script></script>
+<script>
+import webDevData from '../../data/webDevData';
+export default {
+  data() {
+    return {
+      webData: webDevData,
+    };
+  },
+};
+</script>
 
 <style scoped>
 .web {
   position: relative;
   z-index: 1;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  padding: var(--padding-section-default);
 }
+
 .video {
   width: 100vw;
   height: calc(100vh - var(--size-header-height));
@@ -24,5 +52,69 @@
 
   object-fit: cover;
   opacity: 20%;
+}
+
+h1 {
+  font-size: var(--size-font-header-main);
+  margin-bottom: 20px;
+}
+
+h2,
+h3 {
+  font-size: var(--size-font-header-second);
+}
+
+h2 {
+  margin-bottom: 30px;
+}
+h3 {
+  margin-bottom: 10px;
+}
+
+p {
+  display: flex;
+  flex-flow: column;
+  font-size: 20px;
+  margin-bottom: 30px;
+}
+
+ul {
+  padding: 0;
+  list-style: none;
+  display: grid;
+  grid-template-columns: repeat(3, var(--size-web-img));
+  gap: 20px;
+}
+
+li {
+  padding: 10px 10px;
+  border: 1px dashed var(--color-main--light);
+  border-radius: 5px;
+  opacity: 80%;
+  transition: all 0.3s;
+  filter: blur(2px);
+}
+
+li:hover {
+  opacity: 100%;
+  transform: scale(1.2);
+  border: 1px dashed var(--color-main--attract);
+  filter: blur(0);
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  overflow: hidden;
+}
+
+img {
+  display: flex;
+  width: calc(var(--size-web-img) - 40px);
+  height: auto;
 }
 </style>
---
+++ it-service/src/data/webDevData.js
@@ -0,0 +1,33 @@
+import img1 from '../assets/images/web/1.webp';
+import img2 from '../assets/images/web/2.webp';
+import img3 from '../assets/images/web/3.webp';
+import img4 from '../assets/images/web/4.webp';
+import img5 from '../assets/images/web/5.webp';
+import img6 from '../assets/images/web/6.webp';
+import img7 from '../assets/images/web/7.webp';
+import img8 from '../assets/images/web/8.webp';
+import img9 from '../assets/images/web/9.webp';
+import img10 from '../assets/images/web/10.webp';
+import img11 from '../assets/images/web/11.webp';
+import img12 from '../assets/images/web/12.webp';
+import img13 from '../assets/images/web/13.webp';
+import img14 from '../assets/images/web/14.webp';
+import img15 from '../assets/images/web/15.webp';
+
+export default [
+  { name: 'Site 1', link: 'site 1 link', img: img1 },
+  { name: 'Site 2', link: 'site 2 link', img: img2 },
+  { name: 'Site 3', link: 'site 3 link', img: img3 },
+  { name: 'Site 4', link: 'site 4 link', img: img4 },
+  { name: 'Site 5', link: 'site 5 link', img: img5 },
+  { name: 'Site 6', link: 'site 6 link', img: img6 },
+  { name: 'Site 7', link: 'site 7 link', img: img7 },
+  { name: 'Site 8', link: 'site 8 link', img: img8 },
+  { name: 'Site 9', link: 'site 9 link', img: img9 },
+  { name: 'Site 10', link: 'site 10 link', img: img10 },
+  { name: 'Site 11', link: 'site 11 link', img: img11 },
+  { name: 'Site 12', link: 'site 12 link', img: img12 },
+  { name: 'Site 13', link: 'site 13 link', img: img13 },
+  { name: 'Site 14', link: 'site 14 link', img: img14 },
+  { name: 'Site 15', link: 'site 15 link', img: img15 },
+];
