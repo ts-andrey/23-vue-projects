7de1023984a52902c3bb040c45410bd5d901f2c2
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2023-10-09 22:22:07 +0300

    feat: add shop data

---
+++ it-service/src/data/store/phoneData.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    model: 'MEIZU M5',
+    description:
+      'OS: Android. Durable and beautiful; Polycarbonate body; 13-megapixel camera; Phase detection AF; 2GB/3GB RAM; 16GB/32GB internal storage; Dual SIM support; Dual standby mode; 2.5D contoured glass; Fully-laminated 5.2-inch display; mTouch; Fingerprint sensor..',
+    price: '240',
+    image: null,
+  },
+  {
+    model: 'Doogee BL5500 Lite',
+    description:
+      'OS: Android; Processor: Processor (Clock): Quad-Core 1.3GHz ARM Cortex A53; RAM: 2GB; Storage: 16GB; Display: Screen (Size & Type): 6.19 inches IPS, HD+ Display; Camera: Camera Overview (Back/Front): 13 MP + 8 MP / 5 MP; Battery: Non-Removable 5,500 mAh Lithium-Polymer.',
+    price: '170',
+    image: null,
+  },
+  {
+    model: 'Digma Linx X1 3G',
+    description:
+      'OS: Android. Mobile phone Digma Linx X1 3G LS4050MG. Rechargeable battery 2000 mAh Dimensions 71.3 x 142.8 x 9.7 mm. Front Camera 1.92 Mp, 1600 x 1200 pixels, 640 x 480 pixels.',
+    price: '420',
+    image: null,
+  },
+  {
+    model: 'Apple iPhone XR',
+    description:
+      'Released: Oct 26, 2018; Display: 6.1 inches. 1792 x 828 pixels; Camera: 12 MP (Single camera) 7 MP front; Hardware: Apple A12 Bionic. 3GB RAM; Storage: 64GB, not expandable; Battery: 2942 mAh; OS: iOS 15.x.',
+    price: '800',
+    image: null,
+  },
+  {
+    model: 'HONOR 8X JSN-L21',
+    description:
+      'Released: Oct 10, 2018; Display: 6.5 inches. 2340 x 1080 pixels; Camera: 20 MP (Dual camera) 16 MP front; Hardware: HiSilicon Kirin. 4GB RAM; Storage: 128GB, microSDXC; Battery: 3750 mAh;  OS: Android 9.0 Pie. Emotion UI.',
+    price: '300',
+    image: null,
+  },
+  {
+    model: 'Samsung Galaxy S8+',
+    description:
+      'Released: Apr 21, 2017; Display: 5.8 inches. 2960 x 1440 pixels; Camera: 12 MP (Single camera) 8 MP front; Hardware: Qualcomm Snapdragon 835. 4GB RAM; Storage: 64GB, microSDXC; Battery: 3000 mAh; OS: Android 9.0 Pie. Samsung One UI.',
+    price: '500',
+    image: null,
+  },
+];
