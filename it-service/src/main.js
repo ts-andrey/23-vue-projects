ce7cd78ba10b925c6680feae71d7e4a69ed86127
Andrei Tsakunou <andrey.tsakunov@gmail.com>
2023-10-18 17:56:05 +0300

    feat: add vuex to the project

--- it-service/src/main.js
+++ it-service/src/main.js
@@ -3,6 +3,7 @@ import './util/main.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import store from './store/store';
 
 import BaseCard from './ui/BaseCard.vue';
 import BaseLogo from './ui/BaseLogo.vue';
@@ -11,6 +12,8 @@ import BaseButton from './ui/BaseButton.vue';
 const app = createApp(App);
 
 app.use(router);
+app.use(store);
+
 app.component('base-card', BaseCard);
 app.component('base-logo', BaseLogo);
 app.component('base-button', BaseButton);
